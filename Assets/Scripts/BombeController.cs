d15d3f27d62becb28e441d2096c8bc174eb3ebc0
lemontey20 <alice.lemontey@edu.fh-joanneum.at>
2020-12-09 13:08:06 +0100

    Final Assignment

---
+++ Assets/Scripts/BombeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class BombeController : MonoBehaviour
+{
+
+    public GameObject explosionEffect;
+    public float time = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void explose()
+    {
+        ParticleSystem exp = GetComponent<ParticleSystem>();
+        exp.Play();
+        Destroy(gameObject, exp.main.duration);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision avec : " + collision.gameObject.name);
+        //invokeParticleEffect();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [SerializeField] private int m_type;//load in the unity editor which kind of collectable it is
+
+    [SerializeField] private GameObject m_exp = null;
+
+    private void Start() 
+    {
+    m_type = (int)Random.Range(1.0f,3.5f);//load a collectable in a random way between 1 and 3
+    }
+
+    public int GetCollectableType() //precise which type of collectable it is
+    {
+        return(m_type);
+    }
+
+    public void explode()
+    {
+        GameObject _exp = Instantiate(m_exp, transform.position, transform.rotation);
+        Destroy(_exp,3);
+    }
+}
